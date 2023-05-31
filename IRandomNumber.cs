fa550732cfe15bbcbb3fe7abbab466f04687c536
M. Rizal <mrizalhq@gmail.com>
2023-05-31 16:59:24 +0700

    First commit

---
+++ IRandomNumber.cs
@@ -0,0 +1,15 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+
+namespace RandomNumber
+{
+    [OSInterface]
+    public interface IRandomNumber
+    {
+        [OSAction(ReturnName ="RandomInteger")]
+        public int GetRandomInteger(int Min, int Max);
+
+
+        [OSAction(ReturnName = "RandomDecimal")]
+        public decimal GetRandomDecimal(decimal Min, decimal Max);
+    }
+}
---
+++ RandomNumber.cs
@@ -0,0 +1,35 @@
+﻿namespace RandomNumber
+{
+    public class RandomNumber : IRandomNumber
+    {
+        private static Random? _random;
+
+        public decimal GetRandomDecimal(decimal Min, decimal Max)
+        {
+            if (Min > Max || Min < 0)
+            {
+                throw new Exception("Input fields Exception: The min number is 0 and the max can't be lower than min!");
+            }
+            else
+            {
+                double min = (Double)Min;
+                double max = (Double)Max;
+                if (_random == null) _random = new Random();
+                return (Decimal)(_random.NextDouble() * (max - min) + min);
+            }
+        }
+
+        public int GetRandomInteger(int Min, int Max)
+        {
+            if (Min > Max || Min < 0)
+            {
+                throw new Exception("Input fields Exception: The min number is 0 and the max can't be lower than min!");
+            }
+            else
+            {
+                if (_random == null) _random = new Random();
+                return _random.Next(Min, Max + 1);
+            }
+        }
+    }
+}
